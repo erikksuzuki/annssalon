10ae96f4f5f4ac4e3b4bb65f9678a8eb1438ae45
Taisuke Eric Suzuki <erikksuzuki@gmail.com>
2024-08-16 19:11:40 -0700

    Add quote and star SVG icon components

---
+++ src/assets/icons/common/IconQuote.tsx
@@ -0,0 +1,28 @@
+const IconQuote = ({ className = 'w-5 h-5' }: { className?: string }) => {
+  return (
+    <svg
+      fill="currentColor"
+      className={className}
+      version="1.1"
+      id="Layer_1"
+      xmlns="http://www.w3.org/2000/svg"
+      width="800px"
+      height="800px"
+      viewBox="796 698 200 200"
+      enable-background="new 796 698 200 200"
+    >
+      <g>
+        <path
+          d="M885.208,749.739v-40.948C836.019,708.791,796,748.81,796,798v89.209h89.208V798h-48.26
+		C836.948,771.39,858.598,749.739,885.208,749.739z"
+        />
+        <path
+          d="M996,749.739v-40.948c-49.19,0-89.209,40.019-89.209,89.209v89.209H996V798h-48.26
+		C947.74,771.39,969.39,749.739,996,749.739z"
+        />
+      </g>
+    </svg>
+  )
+}
+
+export default IconQuote
