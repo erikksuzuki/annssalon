5ea38df15e84327abd9cac61310812f328b8a6f2
Taisuke Eric Suzuki <erikksuzuki@gmail.com>
2024-08-17 07:28:50 -0700

    Update SubHeading.tsx

--- src/components/common/SubHeading.tsx
+++ src/components/common/SubHeading.tsx
@@ -1,6 +1,21 @@
-const SubHeading = ({ children }: any) => {
+import clsx from 'clsx'
+import { ReactNode } from 'react'
+
+interface SubHeadingProps {
+  children?: ReactNode
+  className?: string
+}
+
+const SubHeading = ({
+  children = 'Heading goes here',
+  className,
+}: SubHeadingProps) => {
   return (
-    <h3 className="uppercase tracking-widest text-sm font-bold">{children}</h3>
+    <h3
+      className={clsx('uppercase tracking-widest text-sm font-bold', className)}
+    >
+      {children}
+    </h3>
   )
 }
 
