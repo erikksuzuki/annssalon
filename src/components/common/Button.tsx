3f1f392ef759176b6052ece28db19911b2f9c806
Taisuke Eric Suzuki <erikksuzuki@gmail.com>
2024-08-16 19:11:50 -0700

    Color adjustments to button

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -21,17 +21,17 @@ const Button = ({
         transitionDuration: '150ms',
         transitionTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)',
         boxShadow: shadow
-          ? '0 10px 15px -3px rgb(194 65 12 / 0.5), 0 4px 6px -4px rgb(194 65 12 / 0.5)'
+          ? '0 10px 15px -3px rgb(194 12 141 / 0.5), 0 4px 6px -4px rgb(194 12 141 / 0.5)'
           : 'none',
       }}
       className={clsx(
-        'mt-4 font-bold rounded-md hover:bg-[#EA580C] hover:text-[#fff]',
+        'mt-4 font-bold rounded-md hover:bg-[#EA0C9E] hover:text-[#fff]',
         { 'text-lg px-9 py-4': size === 'lg' },
         { 'text-base px-8 py-3': size === 'md' },
-        { 'bg-[rgb(194,65,12)]': !secondaryColor },
+        { 'bg-[rgb(194,12,141)]': !secondaryColor },
         { 'text-[#fff]': !secondaryColor },
-        { 'bg-[rgba(234,88,12,0.2)]': secondaryColor },
-        { 'text-[rgb(154,52,18)]': secondaryColor }
+        { 'bg-[rgba(194,12,141,0.2)]': secondaryColor },
+        { 'text-[rgb(154,18,119)]': secondaryColor }
       )}
     >
       Book Now
