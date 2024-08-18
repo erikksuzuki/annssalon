bb22af68bb48ed21b5a17d9af324dbf4d69d44a2
Taisuke Eric Suzuki <erikksuzuki@gmail.com>
2024-08-17 20:14:37 -0700

    Component cleanup

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,11 @@
 import { useMessages, useTranslations } from 'next-intl'
 
-import SocialLinks from '@/components/common/SocialLinks'
-import { socialLinkData } from '../socialLinkData'
-import LanguagePicker from '@/components/common/LanguageSwitcher'
-
-import Button from '@/components/common/Button'
 import GiantQuote from '@/components/Landing/GiantQuote'
 import PhotoQuote from '@/components/Landing/PhotoQuote'
 
-import ShopPhoto from '@/assets/photos/shop.jpg'
-import SubHeading from '@/components/common/SubHeading'
+import 'react-multi-carousel/lib/styles.css'
+import ServiceCarousel from '@/components/Landing/ServiceCarousel'
+import HeroSection from '@/components/Landing/HeroSection'
 
 export default function Home() {
   const t = useTranslations('header')
@@ -17,50 +13,8 @@ export default function Home() {
 
   return (
     <div>
-      <main
-        className="bg-black"
-        style={{
-          backgroundImage: `url(${ShopPhoto.src})`,
-          backgroundPosition: 'center',
-          backgroundSize: 'cover',
-        }}
-      >
-        <div
-          className="w-full h-full"
-          style={{
-            backdropFilter: 'brightness(0.46) contrast(83%)',
-          }}
-        >
-          <section className="text-left py-[180px] px-4 md:px-8 w-full mx-auto max-w-[1280px] relative">
-            <div className="absolute top-4 md:top-24 right-4">
-              <LanguagePicker />
-            </div>
-            <h1 className="text-theme-heading-lg font-sedan">
-              Your One-Stop Shop For All Your
-              <br />
-              Beauty Needs in Las Vegas
-            </h1>
-            <SubHeading className="mt-4">
-              Professional hair, nails and skin care
-            </SubHeading>
-            <Button shadow className="mt-12" size="lg">
-              Book Now
-            </Button>
-          </section>
-        </div>
-        {/* 
-        <section className="text-left mb-8">
-          <h1 className="text-theme-heading-xs font-poppins mb-3">
-            Recent Blog Articles
-          </h1>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-            <div className="p-4 rounded-md border border-white">some content</div>
-            <div className="p-4 rounded-md border border-white">some content</div>
-            <div className="p-4 rounded-md border border-white">some content</div>
-          </div>
-        </section>
-      */}
-      </main>
+      <HeroSection />
+      <ServiceCarousel />
       <PhotoQuote />
       <GiantQuote />
       <div className="py-24 px-4 md:px-8 w-full mx-auto max-w-[1024px]"></div>
--- src/app/socialLinkData.tsx
+++ src/app/socialLinkData.tsx
@@ -15,32 +15,22 @@ export interface SocialLink {
 export const socialLinkData: SocialLink[] = [
   {
     name: 'GitHub',
-    href: 'https://github.com/erikksuzuki/portfolio/pull/1',
+    href: 'https://github.com/erikksuzuki/',
     icon: <IconGithub className="w-6 h-6 hover:text-[#DDDDDD]" />,
   },
   {
     name: 'Instagram',
-    href: 'https://www.instagram.com/erikksuzuki',
+    href: 'https://www.instagram.com/',
     icon: <IconInstagram className="w-6 h-6 hover:text-[#FAA6A0]" />,
   },
   {
     name: 'LinkedIn',
-    href: 'https://www.linkedin.com/in/eric-suzuki-04380449/',
+    href: 'https://www.linkedin.com/',
     icon: <IconLinkedin className="w-6 h-6 hover:text-[#4DA6F0]" />,
   },
   {
     name: 'Tumblr',
-    href: 'https://emptyblueprints-blog.tumblr.com',
+    href: 'https://tumblr.com',
     icon: <IconTumblr className="w-6 h-6 hover:text-[#4DCAE0]" />,
-  },
-  {
-    name: 'Resume',
-    href: 'https://archived.alkemyst.app/eric_suzuki_cv_2024_with_portfolio.pdf',
-    icon: <IconCv className="w-6 h-6 hover:text-[#DDDDDD]" />,
-  },
-  {
-    name: 'Calendly',
-    href: 'https://calendly.com/erikksuzuki/one-hour-block',
-    icon: <IconCalendly className="w-6 h-6 hover:text-[#DDDDDD]" />,
   },
 ]
