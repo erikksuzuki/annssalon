b2e5c50a67ab97f17da33649b3a2654baea8e223
Taisuke Eric Suzuki <erikksuzuki@gmail.com>
2024-08-31 00:54:59 -0700

    add mobile swipe indicator for phones

--- src/components/Landing/ServiceCarousel.tsx
+++ src/components/Landing/ServiceCarousel.tsx
@@ -14,6 +14,9 @@ import ServicePerm from '@/assets/photos/services/perm.jpg'
 import ServiceNails from '@/assets/photos/services/nails.jpg'
 import EyelashExtensions from '@/assets/photos/services/eyelashextensions.jpg'
 
+import SwipePng from '@/assets/swipe.png'
+import Image from 'next/image'
+
 const responsive = {
   '2xl': {
     breakpoint: { min: 2000, max: 4600 },
@@ -41,122 +44,134 @@ const responsive = {
 const ServiceCarousel = () => {
   const { isAboveMd } = useResponsive('md')
   return (
-    <Carousel
-      ssr={true}
-      responsive={responsive}
-      infinite={true}
-      arrows={isAboveMd ? true : false}
-      partialVisible={isAboveMd ? false : true}
-      centerMode={isAboveMd ? true : false}
-    >
-      <ServiceCard
-        className="mb-0"
-        imgSrc={ServiceMensHairCutPhoto.src}
-        subHeading="Starting at $23"
-        heading="Men's Hair Cut"
-        dialogContent={[
-          { name: "Men's Hair Cut", price: 23 },
-          { name: 'Hair Cut & Shampoo', price: 30 },
-          { name: 'Hair Cut & Color', price: 65 },
-        ]}
+    <section className="relative">
+      <Carousel
+        ssr={true}
+        responsive={responsive}
+        infinite={true}
+        arrows={isAboveMd ? true : false}
+        partialVisible={isAboveMd ? false : true}
+        centerMode={isAboveMd ? true : false}
       >
-        Two reasons you should find a stylist and stick with them, consistency
-        and efficiency. As we get to know your hair better, your cut gets
-        better, and we get faster. It&apos;s that simple.
-      </ServiceCard>
-      <ServiceCard
-        className="mb-0"
-        imgSrc={ServiceWomansHairCutPhoto.src}
-        subHeading="Starting at $30"
-        heading="Women's Haircut"
-        dialogContent={[
-          { name: 'Long Haircut & Layer', price: 40 },
-          { name: 'Medium Haircut & Layer', price: 33 },
-          { name: 'Short Haircut', price: 30 },
-          { name: 'Shampoo for Long Hair', price: 30 },
-          { name: 'Shampoo & Blowdry', price: 25 },
-          { name: 'Hot oil treatment', price: 25 },
-        ]}
-      >
-        Two reasons you should find a stylist and stick with them, consistency
-        and efficiency. As we get to know your hair better, your cut gets
-        better, and we get faster. It&apos;s that simple.
-      </ServiceCard>
-      <ServiceCard
-        className="mb-0"
-        imgSrc={ServiceHairColor.src}
-        subHeading="Starting at $60"
-        heading="Hair Highlighting"
-        dialogContent={[
-          { name: 'Long Hair', price: 100 },
-          { name: 'Medium Hair', price: 80 },
-          { name: 'Short Hair', price: 65 },
-          { name: 'Root Touchup', price: 60 },
-        ]}
-      >
-        Two reasons you should find a stylist and stick with them, consistency
-        and efficiency. As we get to know your hair better, your cut gets
-        better, and we get faster. It&apos;s that simple.
-      </ServiceCard>
-      <ServiceCard
-        className="mb-0"
-        imgSrc={ServiceHighlightColor.src}
-        subHeading="Starting at $90"
-        heading="Hair Highlighting"
-        dialogContent={[
-          { name: 'Long Hair', price: 160 },
-          { name: 'Medium Hair', price: 120 },
-          { name: 'Short Hair', price: 90 },
-        ]}
-      >
-        Two reasons you should find a stylist and stick with them, consistency
-        and efficiency. As we get to know your hair better, your cut gets
-        better, and we get faster. It&apos;s that simple.
-      </ServiceCard>
-      <ServiceCard
-        className="mb-0"
-        imgSrc={ServicePerm.src}
-        subHeading="Starting at $70"
-        heading="Hair Perming"
-        dialogContent={[
-          { name: 'Long Hair', price: 110 },
-          { name: 'Short Hair', price: 70 },
-        ]}
-      >
-        Two reasons you should find a stylist and stick with them, consistency
-        and efficiency. As we get to know your hair better, your cut gets
-        better, and we get faster. It&apos;s that simple.
-      </ServiceCard>
-      <ServiceCard
-        className="mb-0"
-        imgSrc={ServiceNails.src}
-        subHeading="Starting at $20"
-        heading="Pedicures and Manicures"
-        dialogContent={[
-          { name: 'Manicure', price: 20 },
-          { name: 'Gel Manicure', price: 35 },
-          { name: 'Pedicure', price: 30 },
-        ]}
-      >
-        Two reasons you should find a stylist and stick with them, consistency
-        and efficiency. As we get to know your hair better, your cut gets
-        better, and we get faster. It&apos;s that simple.
-      </ServiceCard>
-      <ServiceCard
-        className="mb-0"
-        imgSrc={EyelashExtensions.src}
-        subHeading="Starting at $70"
-        heading="Eyelash Extensions"
-        dialogContent={[
-          { name: 'Full set', price: 120 },
-          { name: 'Retouch', price: 70 },
-        ]}
-      >
-        Two reasons you should find a stylist and stick with them, consistency
-        and efficiency. As we get to know your hair better, your cut gets
-        better, and we get faster. It&apos;s that simple.
-      </ServiceCard>
-    </Carousel>
+        <ServiceCard
+          imgSrc={ServiceWomansHairCutPhoto.src}
+          subHeading="Starting at $30"
+          heading="Women's Haircut"
+          dialogContent={[
+            { name: 'Long Haircut & Layer', price: 40 },
+            { name: 'Medium Haircut & Layer', price: 33 },
+            { name: 'Short Haircut', price: 30 },
+            { name: 'Shampoo for Long Hair', price: 30 },
+            { name: 'Shampoo & Blowdry', price: 25 },
+            { name: 'Hot oil treatment', price: 25 },
+          ]}
+        >
+          Expert stylists provide a personalized haircut designed to enhance
+          natural features and hair texture. Includes a thorough consultation,
+          precision cutting, and styling, resulting in a flattering and
+          manageable look that reflects your style.
+        </ServiceCard>
+        <ServiceCard
+          imgSrc={ServiceHighlightColor.src}
+          subHeading="Starting at $90"
+          heading="Hair Highlighting"
+          dialogContent={[
+            { name: 'Long Hair', price: 160 },
+            { name: 'Medium Hair', price: 120 },
+            { name: 'Short Hair', price: 90 },
+          ]}
+        >
+          Enhance your hair's dimension with customized highlights or balayage.
+          This service adds depth and light, with subtle or bold effects, using
+          techniques that blend seamlessly with your natural hair for a
+          sun-kissed, modern finish.
+        </ServiceCard>
+        <ServiceCard
+          imgSrc={ServiceHairColor.src}
+          subHeading="Starting at $60"
+          heading="Hair Highlighting"
+          dialogContent={[
+            { name: 'Long Hair', price: 100 },
+            { name: 'Medium Hair', price: 80 },
+            { name: 'Short Hair', price: 65 },
+            { name: 'Root Touchup', price: 60 },
+          ]}
+        >
+          Transform your look with professional hair coloring services. Whether
+          covering grays or achieving a bold new hue, our colorists use
+          top-quality products to ensure vibrant, long-lasting results tailored
+          to your skin tone and style.
+        </ServiceCard>
+
+        <ServiceCard
+          imgSrc={ServiceMensHairCutPhoto.src}
+          subHeading="Starting at $23"
+          heading="Men's Hair Cut"
+          dialogContent={[
+            { name: "Men's Hair Cut", price: 23 },
+            { name: 'Hair Cut & Shampoo', price: 30 },
+            { name: 'Hair Cut & Color', price: 65 },
+          ]}
+        >
+          A precise and stylish cut tailored to each client’s preferences,
+          considering hair type and lifestyle. The service includes
+          consultation, cut, and finish, ensuring a polished look that suits
+          individual tastes.
+        </ServiceCard>
+        <ServiceCard
+          imgSrc={ServicePerm.src}
+          subHeading="Starting at $70"
+          heading="Hair Perming"
+          dialogContent={[
+            { name: 'Long Hair', price: 110 },
+            { name: 'Short Hair', price: 70 },
+          ]}
+        >
+          Add volume and texture with professional perming services. Our
+          stylists use gentle techniques to create natural-looking curls or
+          waves, tailored to your hair type, for a bouncy, resilient style that
+          lasts.
+        </ServiceCard>
+        <ServiceCard
+          imgSrc={ServiceNails.src}
+          subHeading="Starting at $20"
+          heading="Pedicures and Manicures"
+          dialogContent={[
+            { name: 'Manicure', price: 20 },
+            { name: 'Gel Manicure', price: 35 },
+            { name: 'Pedicure', price: 30 },
+          ]}
+        >
+          Pamper your hands and feet with our manicure and pedicure services.
+          Enjoy meticulous nail shaping, cuticle care, and polish application in
+          a relaxing environment, leaving your nails looking polished and
+          perfectly groomed.
+        </ServiceCard>
+        <ServiceCard
+          imgSrc={EyelashExtensions.src}
+          subHeading="Starting at $70"
+          heading="Eyelash Extensions"
+          dialogContent={[
+            { name: 'Full set', price: 120 },
+            { name: 'Retouch', price: 70 },
+          ]}
+        >
+          Enhance your eyes with expertly applied eyelash extensions. Our
+          technicians customize length, curl, and volume to create a natural or
+          dramatic look, using high-quality lashes that are lightweight,
+          comfortable, and long-lasting for a beautiful finish.
+        </ServiceCard>
+      </Carousel>
+      <div className="text-center opacity-[0.4] block md:hidden absolute bottom-[0px] w-full z-[-100]">
+        <Image
+          className="inline-block"
+          src={SwipePng.src}
+          width={200}
+          height={200}
+          alt={'swipe'}
+        />
+      </div>
+    </section>
   )
 }
 
